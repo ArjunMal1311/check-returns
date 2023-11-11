10c3f04db255e4f221a9c7e730ecaf3ffd4ddc9b
Arjun Malhotra <hiarjun.malhotra2002@gmail.com>
2023-11-11 17:27:25 +0530

    misc

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -18,7 +18,7 @@ export const authOptions: AuthOptions = {
                 email: { label: 'email', type: 'text' },
                 password: { label: 'password', type: 'password' }
             },
-
+            
             async authorize(credentials) {
                 if (!credentials?.email || !credentials?.password) {
                     throw new Error('Invalid credentials');
